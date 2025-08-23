7d5768808d0b07356f5947cc25dff00eab292fdd
Albin Baby <albinbtg@gmail.com>
2025-08-23 16:18:38 +0530

    basic structure

--- bite-speed-frontend/src/App.tsx
+++ bite-speed-frontend/src/App.tsx
@@ -1,9 +1,20 @@
-
 function App() {
-  return <div>
-    <p className=" bg-black text-red-400">Hello world</p>
+  return (
+    <div className=" grid grid-cols-[1fr_350px] h-screen w-full">
+      {/* the chat bot flow  */}
+      <div></div>
 
-  </div>
+      {/* panel */}
+      <div className=" bg-gray-200 h-full w-full grid grid-rows-[1fr_auto]">
+        <div>
+          <p className=" p-2 text-lg font-bold text-center  border-b border-black/40 ">Chat-Bot Flow Panel</p>
+        </div>
+        <button className=" bg-black/80 rounded-sm text-white h-10 m-2 font-bold cursor-pointer">
+          Save Changes
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
